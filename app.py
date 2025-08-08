17640a6490e95874c1e998730070f113b8f58a08
Ahmed-Saleh-DSV <ahmed.saleh.saleh@dsv.com>
2025-08-08 11:57:53 +0400

    Update app.py

--- app.py
+++ app.py
@@ -270,7 +270,8 @@ def generate_transport():
     return send_file(file_stream, as_attachment=True, download_name='TransportQuotation.docx')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
  
 @app.route("/chat", methods=["POST"])
 def chat():
